ee820aff7dfa3add1cae919ad9628215231b4e57
moirao <90628406+moirao@users.noreply.github.com>
2021-07-11 16:45:08 +0000

    Updated package versions.

--- blender/__init__.py
+++ blender/__init__.py
@@ -1,3 +1,6 @@
 bl_info = {
     'name': 'Taichi Elements',
-  
+    'description': 'High-Performance Multi-Material Continuum Physics Engine',
+    'author': 'Taichi Elements Developers',
+    'version': (0, 0, 0),
+    'bl

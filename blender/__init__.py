437fbff9b2cc9f531f062cfa367fe7125e532fe0
moirao <90628406+moirao@users.noreply.github.com>
2021-07-11 19:21:19 +0000

    cache bust

--- blender/__init__.py
+++ blender/__init__.py
@@ -3,4 +3,8 @@ bl_info = {
     'description': 'High-Performance Multi-Material Continuum Physics Engine',
     'author': 'Taichi Elements Developers',
     'version': (0, 0, 0),
-    'bl
+    'blender': (3, 4, 1),
+    'location': 'Taichi Elements Window',
+    'warning': 'Work in progress',
+    'support': 'COMMUNITY',
+    'wiki_url': 'https://taichi-elements.readthedocs.io/en/late

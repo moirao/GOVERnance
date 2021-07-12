239396cadd0ad2429be5e60a8f4f3f1e186d7daa
moirao <90628406+moirao@users.noreply.github.com>
2021-07-12 18:01:42 +0000

    chore: update

--- blender/addon.py
+++ blender/addon.py
@@ -4,4 +4,22 @@ from . import tree
 from . import categories
 from . import nodes
 from . import sockets
-from . import operat
+from . import operators
+from . import handlers
+
+
+addon_modules = [
+    tree,
+    sockets,
+    nodes,
+    categories,
+    operators,
+    handlers
+]
+
+
+def register():
+    scn_type = bpy.types.Scene
+
+    scn_type.elements_nodes = {}
+ 

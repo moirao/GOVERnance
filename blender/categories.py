6ed38dd5c281e8b4e8e515903db79841978c562e
moirao <90628406+moirao@users.noreply.github.com>
2021-07-14 12:20:09 +0000

    fix sorting

--- blender/categories.py
+++ blender/categories.py
@@ -3,4 +3,12 @@ import nodeitems_utils
 from . import nodes
 
 
-class ElementsNodeCategory(nodeitems_utils.
+class ElementsNodeCategory(nodeitems_utils.NodeCategory):
+    @classmethod
+    def poll(cls, context):
+        return context.space_data.tree_type == 'elements_node_tree'
+
+
+# get categories data
+def get_categs_data():
+    # nod

35709c2cf050376f63552eba52578a255b95062d
moirao <90628406+moirao@users.noreply.github.com>
2021-07-16 12:13:50 +0000

    Cleaned code.

--- blender/nodes/__init__.py
+++ blender/nodes/__init__.py
@@ -1,3 +1,13 @@
 import inspect
 
 import bpy
+
+from .base import *
+
+# categories
+from .inputs import *
+from .output import *
+from .component import *
+from .color import *
+from .converter import *
+from .struct 

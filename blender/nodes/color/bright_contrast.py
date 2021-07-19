432236ab44872012227175e502d14bea11811e3d
moirao <90628406+moirao@users.noreply.github.com>
2021-07-19 09:11:20 +0000

    Initial checkin for full screen functionality.

--- blender/nodes/color/bright_contrast.py
+++ blender/nodes/color/bright_contrast.py
@@ -9,4 +9,10 @@ def get_out_value(socket):
     # input color
     in_col = node.inputs['Color'].get_value()
     # bright
-    brg = node.inputs
+    brg = node.inputs['Bright'].get_value()[0]
+    # contrast
+    cntr = node.inputs['Contrast'].get_value()[0]
+    # scene
+    scn = bpy.context.scene
+    key = '{0}.{1}'.format(node.name, out.name)
+    # resul

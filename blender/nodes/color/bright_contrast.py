1816b548931e08e094ad64ddc7bc6c695392e013
moirao <90628406+moirao@users.noreply.github.com>
2021-07-19 08:32:42 +0000

    modified:   .gitignore

--- blender/nodes/color/bright_contrast.py
+++ blender/nodes/color/bright_contrast.py
@@ -3,4 +3,10 @@ import bpy
 from .. import base
 
 
-def 
+def get_out_value(socket):
+    node = socket.node
+    out = node.outputs['Color']
+    # input color
+    in_col = node.inputs['Color'].get_value()
+    # bright
+    brg = node.inputs

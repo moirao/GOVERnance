e87713fc529e6c504f3c1af68f8acd0f63668972
moirao <90628406+moirao@users.noreply.github.com>
2021-07-22 12:32:39 +0000

    :fireworks:

--- blender/nodes/color/mix_rgb.py
+++ blender/nodes/color/mix_rgb.py
@@ -5,4 +5,8 @@ from .. import base
 
 def mix_rgb(c1, c2, mode):
     if mode == 'ADD':
-        res = (c1[0] + c2[0], c1[1] + c2[1
+        res = (c1[0] + c2[0], c1[1] + c2[1], c1[2] + c2[2])
+    elif mode == 'MULTIPLY':
+        res = (c1[0] * c2[0], c1[1] * c2[1], c1[2] * c2[2])
+    elif mode == 'SUBTRACT':
+        res = (c1[0] - c2[0], c1[1] - c

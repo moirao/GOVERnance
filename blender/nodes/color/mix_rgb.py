e6b231f22b3944868755cfe5029105af29a75ab2
moirao <90628406+moirao@users.noreply.github.com>
2021-07-23 08:07:32 +0000

    resolved conflicts

--- blender/nodes/color/mix_rgb.py
+++ blender/nodes/color/mix_rgb.py
@@ -9,4 +9,10 @@ def mix_rgb(c1, c2, mode):
     elif mode == 'MULTIPLY':
         res = (c1[0] * c2[0], c1[1] * c2[1], c1[2] * c2[2])
     elif mode == 'SUBTRACT':
-        res = (c1[0] - c2[0], c1[1] - c
+        res = (c1[0] - c2[0], c1[1] - c2[1], c1[2] - c2[2])
+    elif mode == 'DIVIDE':
+        res = [c1[0], c1[1], c1[2]]
+        if c2[0] != 0.0:
+            res[0] = c1[0] / c2[0]
+        if c2[1] != 0.0:
+            res[1

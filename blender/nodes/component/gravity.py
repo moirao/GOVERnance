879edb98741d5af30b932afdb8ad2ed6b4343c96
moirao <90628406+moirao@users.noreply.github.com>
2021-07-27 19:54:55 +0000

    rwsem: shorter spinlocked section in rwsem_down_failed_common()

--- blender/nodes/component/gravity.py
+++ blender/nodes/component/gravity.py
@@ -5,4 +5,12 @@ class ElementsGravityNode(base.BaseNode):
     bl_idname = 'elements_gravity_node'
     bl_label = 'Gravity'
 
-    category = base
+    category = base.COMPONENT
+
+    def init(self, context):
+        self.width = 190.0
+
+        out = self.outputs.new('elements_struct_socket', 'Gravity')
+        out.text = 'Gravity Force'
+
+        dir

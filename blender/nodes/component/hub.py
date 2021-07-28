089d8baf2855f6b04697ad9abf39d9e29b70d2e0
moirao <90628406+moirao@users.noreply.github.com>
2021-07-28 14:00:29 +0000

    Updated spanish translation

--- blender/nodes/component/hub.py
+++ blender/nodes/component/hub.py
@@ -1,1 +1,10 @@
-from .. imp
+from .. import base
+
+
+class ElementsHubNode(base.BaseNode):
+    bl_idname = 'elements_hub_node'
+    bl_label = 'Hub'
+
+    required_nodes = {
+        'Forces': [
+            'elements_gr

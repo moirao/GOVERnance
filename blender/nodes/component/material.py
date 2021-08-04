6823b145d4eb6a9c8a3294ab016b09b6bdccd25a
moirao <90628406+moirao@users.noreply.github.com>
2021-08-04 17:21:36 +0000

    testing things

--- blender/nodes/component/material.py
+++ blender/nodes/component/material.py
@@ -7,4 +7,9 @@ class ElementsMaterialNode(base.BaseNode):
     bl_idname = 'elements_material_node'
     bl_label = 'Material'
 
-  
+    items = [
+        ('WATER', 'Water', ''),
+        ('SNOW', 'Snow', ''),
+        ('ELASTIC', 'Elastic', ''),
+        ('SAND', 'Sand', ''),
+        ('STATIONARY', 'Stationary

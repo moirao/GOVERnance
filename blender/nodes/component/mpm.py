5bdc37de3e62522433e04c4bd332822cc0ee2ec5
moirao <90628406+moirao@users.noreply.github.com>
2021-08-06 19:34:49 +0000

    Update navigation

--- blender/nodes/component/mpm.py
+++ blender/nodes/component/mpm.py
@@ -1,4 +1,13 @@
 from .. import base
 
 
-class ElementsMpmSolverNode(base
+class ElementsMpmSolverNode(base.BaseNode):
+    bl_idname = 'elements_mpm_solver_node'
+    bl_label = 'MPM Solver'
+
+    category = base.COMPONENT
+
+    def init(self, context):
+        self.width = 180.0
+
+    

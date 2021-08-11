577b1e8f35eb66ce07d4fa5d877e6b77a5f4c0bd
moirao <90628406+moirao@users.noreply.github.com>
2021-08-11 16:57:37 +0000

    lalal

--- blender/nodes/converter/combine_hsv.py
+++ blender/nodes/converter/combine_hsv.py
@@ -6,4 +6,9 @@ from .. import base
 
 def get_out_value(socket):
     node = socket.node
-    out
+    out = node.outputs['Color']
+    node = socket.node
+    h = node.inputs['H'].get_value()
+    s = node.inputs['S'].get_value()
+    v = node.inputs['V'].get_value()
+    key = '{0}.

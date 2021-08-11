fd9d73b68b57e94c403e06ca015b18d015315935
moirao <90628406+moirao@users.noreply.github.com>
2021-08-11 17:22:49 +0000

    Adding Random Frames

--- blender/nodes/converter/combine_hsv.py
+++ blender/nodes/converter/combine_hsv.py
@@ -11,4 +11,11 @@ def get_out_value(socket):
     h = node.inputs['H'].get_value()
     s = node.inputs['S'].get_value()
     v = node.inputs['V'].get_value()
-    key = '{0}.
+    key = '{0}.{1}'.format(node.name, out.name)
+    res = []
+    for h_val, s_val, v_val in zip(h, s, v):
+        color = mathutils.Color()
+        color.v = v_val
+        color.s = s_val
+        color.h = h_val
+  

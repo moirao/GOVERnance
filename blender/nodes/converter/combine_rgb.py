36fa71fbcc44b7b704e6c6cff239ad7ae90934ea
moirao <90628406+moirao@users.noreply.github.com>
2021-08-14 21:44:23 +0000

    fixed template

--- blender/nodes/converter/combine_rgb.py
+++ blender/nodes/converter/combine_rgb.py
@@ -13,4 +13,11 @@ def get_out_value(socket):
     b = node.inputs['B'].get_value()
     key = '{0}.{1}'.format(node.name, out.name)
     res = []
-    for r_val, g_val, b_val in zip
+    for r_val, g_val, b_val in zip(r, g, b):
+        color = mathutils.Color((r_val, g_val, b_val))
+        res.append(color)
+    scn = bpy.context.scene
+    scn.elements_sockets[key] = res
+
+
+class ElementsCo

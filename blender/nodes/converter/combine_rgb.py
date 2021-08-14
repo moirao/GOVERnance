8224c9ae53fa7eff4b9db4e5030b7f511a29da7c
moirao <90628406+moirao@users.noreply.github.com>
2021-08-14 11:52:21 +0000

    <br>

--- blender/nodes/converter/combine_rgb.py
+++ blender/nodes/converter/combine_rgb.py
@@ -1,1 +1,11 @@
-imp
+import bpy
+import mathutils
+
+from .. import base
+
+
+def get_out_value(socket):
+    node = socket.node
+    out = node.outputs['Color']
+    node = socket.node
+    r = node.inputs[

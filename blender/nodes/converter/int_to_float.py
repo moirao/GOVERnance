d2c8d81adeb4fe846ff0e82980246a4f9783a05a
moirao <90628406+moirao@users.noreply.github.com>
2021-08-17 11:46:33 +0000

    add forgotten file

--- blender/nodes/converter/int_to_float.py
+++ blender/nodes/converter/int_to_float.py
@@ -6,4 +6,11 @@ from .. import base
 def get_out_value(socket):
     node = socket.node
     out = node.outputs['Float']
-    integer = node.inputs['Intege
+    integer = node.inputs['Integer'].get_value()
+    # scene
+    scn = bpy.context.scene
+    key = '{0}.{1}'.format(node.name, out.name)
+    res = []
+    for int_input in integer:
+        res.append(float(int_input))
+ 

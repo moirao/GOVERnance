b3ded7e46f6bf4835b237334ef9342af2470ee14
moirao <90628406+moirao@users.noreply.github.com>
2021-08-23 17:49:50 +0000

    Version 1.3.5

--- blender/nodes/converter/vector_math.py
+++ blender/nodes/converter/vector_math.py
@@ -5,4 +5,10 @@ from .. import base
 
 
 def get_val(mode, val_1, val_2):
-    val_1 = mathutils.Vector(val
+    val_1 = mathutils.Vector(val_1)
+    val_2 = mathutils.Vector(val_2)
+    out = mathutils.Vector()
+    if mode == 'ADD':
+        out = val_1 + val_2
+    elif mode == 'SUBTRACT':
+        out = val_1 - val_

887b3aa5d022fdd80a0a58842d3e4c7fe7f4b71f
moirao <90628406+moirao@users.noreply.github.com>
2021-08-24 07:13:51 +0000

    Removed unneeded line

--- blender/nodes/converter/vector_math.py
+++ blender/nodes/converter/vector_math.py
@@ -11,4 +11,10 @@ def get_val(mode, val_1, val_2):
     if mode == 'ADD':
         out = val_1 + val_2
     elif mode == 'SUBTRACT':
-        out = val_1 - val_
+        out = val_1 - val_2
+    elif mode == 'MULTIPLY':
+        out[0] = val_1[0] * val_2[0]
+        out[1] = val_1[1] * val_2[1]
+        out[2] = val_1[2] * val_2[2]
+    elif mode == 'DIVIDE':
+        out[0] = val_

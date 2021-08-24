b43f3f063c0719144d5ebf21ec120c43eeb6f489
moirao <90628406+moirao@users.noreply.github.com>
2021-08-24 12:15:56 +0000

    Announcement

--- blender/nodes/converter/vector_math.py
+++ blender/nodes/converter/vector_math.py
@@ -17,4 +17,13 @@ def get_val(mode, val_1, val_2):
         out[1] = val_1[1] * val_2[1]
         out[2] = val_1[2] * val_2[2]
     elif mode == 'DIVIDE':
-        out[0] = val_
+        out[0] = val_1[0] / val_2[0]
+        out[1] = val_1[1] / val_2[1]
+        out[2] = val_1[2] / val_2[2]
+    return out
+
+
+def get_res_value(socket):
+    node = socket.node
+    out = node.outputs['Result']
+    va

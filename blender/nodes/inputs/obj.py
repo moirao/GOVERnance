c1a7b46c376db22e0ad2225417a4015063e6b180
moirao <90628406+moirao@users.noreply.github.com>
2021-08-31 17:49:53 +0000

    Words.

--- blender/nodes/inputs/obj.py
+++ blender/nodes/inputs/obj.py
@@ -3,4 +3,14 @@ import bpy
 from .. import base
 
 
-class ElementsSourceObjectNode(base.BaseNode
+class ElementsSourceObjectNode(base.BaseNode):
+    bl_idname = 'elements_source_object_node'
+    bl_label = 'Object'
+
+    obj_name: bpy.props.StringProperty()
+    category = base.INPUT
+
+    def init(self, context):
+        self.width = 180.0
+
+        out = 

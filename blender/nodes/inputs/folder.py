e330d21646d65d21428b61ed3ee448aa4fc4200e
moirao <90628406+moirao@users.noreply.github.com>
2021-08-31 06:05:03 +0000

    DONE MUST TEST CELERY TASK  QUEUE

--- blender/nodes/inputs/folder.py
+++ blender/nodes/inputs/folder.py
@@ -1,1 +1,11 @@
-from .. import bas
+from .. import base
+
+
+class ElementsFolderNode(base.BaseNode):
+    bl_idname = 'elements_folder_node'
+    bl_label = 'Folder'
+
+    category = base.INPUT
+
+    def init(self, context):
+        self.width = 250

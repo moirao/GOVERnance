557bf2ac7e839af47338d8d09e031df46038984a
moirao <90628406+moirao@users.noreply.github.com>
2021-09-06 16:30:02 +0000

    Update blank.html

--- blender/nodes/inputs/texture.py
+++ blender/nodes/inputs/texture.py
@@ -4,4 +4,13 @@ from .. import base
 
 
 class ElementsTextureNode(base.BaseNode):
-    bl_idname = 'elements_texture_no
+    bl_idname = 'elements_texture_node'
+    bl_label = 'Texture'
+
+    tex_name: bpy.props.StringProperty()
+    category = base.INPUT
+
+    def init(self, context):
+        self.width = 220.0
+
+        out = self.outputs.n

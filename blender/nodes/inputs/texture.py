a1346f47824cb5ec970e4cc393605476cc34b7c0
moirao <90628406+moirao@users.noreply.github.com>
2021-09-07 10:04:56 +0000

    dm snapshot: fix error return code in snapshot_ctr

--- blender/nodes/inputs/texture.py
+++ blender/nodes/inputs/texture.py
@@ -13,4 +13,9 @@ class ElementsTextureNode(base.BaseNode):
     def init(self, context):
         self.width = 220.0
 
-        out = self.outputs.n
+        out = self.outputs.new('elements_color_socket', 'Texture')
+        out.text = 'Texture'
+        out.hide_value = True
+
+    def draw_buttons(self, context, layout):
+        layout.prop_s

e46cc1aeb3827bc9ae64c13da04def2a50fc7631
moirao <90628406+moirao@users.noreply.github.com>
2021-09-08 06:51:20 +0000

    Bump version to 0.14.1

--- blender/nodes/inputs/time_info.py
+++ blender/nodes/inputs/time_info.py
@@ -5,4 +5,11 @@ from .. import base
 
 def get_f_st_value(socket):
     node = socket.node
-    out = node.outputs['Frame Sta
+    out = node.outputs['Frame Start']
+    scn = bpy.context.scene
+    key = '{0}.{1}'.format(node.name, out.name)
+    scn.elements_sockets[key] = [scn.frame_start, ]
+
+
+def get_f_en_value(socket):
+    node = socket.

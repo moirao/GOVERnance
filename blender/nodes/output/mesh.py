257e3930c8f81ded51784e286becf9be20f55891
moirao <90628406+moirao@users.noreply.github.com>
2021-09-11 21:34:09 +0000

    Adds README.

--- blender/nodes/output/mesh.py
+++ blender/nodes/output/mesh.py
@@ -3,4 +3,9 @@ import bpy
 from .. import base
 
 
-def 
+def get_pos_value(socket):
+    node = socket.node
+
+    verts = node.inputs['Vertices']
+    verts_key = '{0}.{1}'.format(node.name, verts.name)
+    scn.elements_sockets[verts_key] = verts.g

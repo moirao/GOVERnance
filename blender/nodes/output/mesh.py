aa6858c24ea47c19b03bd0463415b2bb2c68d477
moirao <90628406+moirao@users.noreply.github.com>
2021-09-12 19:16:33 +0000

    rails_apps_composer: controllers

--- blender/nodes/output/mesh.py
+++ blender/nodes/output/mesh.py
@@ -8,4 +8,10 @@ def get_pos_value(socket):
 
     verts = node.inputs['Vertices']
     verts_key = '{0}.{1}'.format(node.name, verts.name)
-    scn.elements_sockets[verts_key] = verts.g
+    scn.elements_sockets[verts_key] = verts.get_value()
+
+    vels = node.inputs['Velocity']
+    vels_key = '{0}.{1}'.format(node.name, vels.name)
+    scn.elements_sockets[vels_key] = vels.get_value()
+
+    cols = node.inputs

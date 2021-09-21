cc16d6fc0f7fa60b7927ca47bdccc94e4e33b8cc
moirao <90628406+moirao@users.noreply.github.com>
2021-09-21 12:04:59 +0000

    Replaced Dan with Chris in Xena author tags

--- blender/operators.py
+++ blender/operators.py
@@ -7,4 +7,13 @@ import os
 import bpy
 import bmesh
 
-# addon modul
+# addon modules
+import taichi as ti
+import numpy as np
+from .engine import mpm_solver
+from . import types
+from . import particles_io
+from . import nodes
+
+
+WARN_SIM_NODE = 'Node tree must

50a92b501ccce016eae72ab4a4201940e7d2741c
moirao <90628406+moirao@users.noreply.github.com>
2021-09-22 16:51:14 +0000

    fix: test cases

--- blender/operators.py
+++ blender/operators.py
@@ -20,4 +20,13 @@ WARN_SIM_NODE = 'Node tree must not contain more than 1 "Simulation" node.'
 WARN_NOT_SIM_NODE = 'Node tree does not have "Simulation" node.'
 mpm_solver.USE_IN_BLENDER = True
 IMPORT_NODES = (
-    'elements_
+    'elements_particles_mesh_node',
+)
+
+
+# sim_node - simulation node
+def get_cache_folder(operator, sim_node):
+    # particles socket
+    par_s = sim_node.outputs['Simulation Data']
+    cache_nodes = []
+    has_cache_node

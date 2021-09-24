ed3d8c6eba4e77db06419bf60baecb1825c3a7b6
moirao <90628406+moirao@users.noreply.github.com>
2021-09-24 21:18:20 +0000

    permutations

--- blender/operators.py
+++ blender/operators.py
@@ -64,4 +64,7 @@ def get_tree_obj(node_tree):
     tree = types.Tree()
 
     for node in node_tree.nodes:
-        if n
+        if node.bl_idname == 'elements_simulation_node':
+            tree.sim_nds[node.name] = node
+        elif node.bl_idname in IMPORT_NODES:
+            if node.bl_idname == 'elements_particles_mesh_node':

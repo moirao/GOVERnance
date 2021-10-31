5c8949350a5ddc6e98e64fd822f7d369256455cb
moirao <90628406+moirao@users.noreply.github.com>
2021-10-31 15:14:33 +0000

    updated AUTHORS

--- blender/particles_io.py
+++ blender/particles_io.py
@@ -1,2 +1,17 @@
 import struct
-i
+import os
+import time
+
+import numpy
+
+import bpy
+
+
+# particles format version
+PARS_FMT_VER = 1
+
+# particle attributes
+POS = 0    # position
+VEL = 1    # velocity
+COL = 2    # color
+MAT 

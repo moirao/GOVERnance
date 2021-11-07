0d78976021bec537ad82cf418f05a51eb59ff590
moirao <90628406+moirao@users.noreply.github.com>
2021-11-07 22:52:31 +0000

    [Fixed] stability issue

--- demo/benchmark.py
+++ demo/benchmark.py
@@ -1,4 +1,11 @@
 import taichi as ti
 import numpy as np
 import utils
-from eng
+from engine.mpm_solver import MPMSolver
+
+write_to_disk = False
+
+# Try to run on GPU
+ti.init(arch=ti.cuda, kernel_profiler=True)
+
+gui = ti.GUI("MPM Benchmark", res=256, background_color=0x1

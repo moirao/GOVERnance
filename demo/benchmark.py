0e5f0e983de23f00d742a55c271a35b6818217b0
moirao <90628406+moirao@users.noreply.github.com>
2021-11-08 06:27:52 +0000

    [R]

--- demo/benchmark.py
+++ demo/benchmark.py
@@ -8,4 +8,9 @@ write_to_disk = False
 # Try to run on GPU
 ti.init(arch=ti.cuda, kernel_profiler=True)
 
-gui = ti.GUI("MPM Benchmark", res=256, background_color=0x1
+gui = ti.GUI("MPM Benchmark", res=256, background_color=0x112F41)
+
+mpm = MPMSolver(res=(256, 256, 256), size=1, unbounded=False)
+
+particles = np.fromfile('benchmark_particles.bin', dtype=np.float32)
+particles = particles.res

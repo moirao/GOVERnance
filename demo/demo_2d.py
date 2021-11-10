a6a4e3de2c6d3c03b27581d976a57ee1669c6c17
moirao <90628406+moirao@users.noreply.github.com>
2021-11-10 14:32:21 +0000

    Changes based on code review

--- demo/demo_2d.py
+++ demo/demo_2d.py
@@ -1,4 +1,12 @@
 import os
 import taichi as ti
 import numpy as np
-impor
+import utils
+import math
+from engine.mpm_solver import MPMSolver
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-o', '--out-dir',

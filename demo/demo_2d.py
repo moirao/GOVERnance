0d07a5b615c1ee530950412260ce54b189995561
moirao <90628406+moirao@users.noreply.github.com>
2021-11-11 15:51:45 +0000

    sockets

--- demo/demo_2d.py
+++ demo/demo_2d.py
@@ -28,4 +28,9 @@ gui = ti.GUI("Taichi Elements", res=512, background_color=0x112F41)
 mpm = MPMSolver(res=(128, 128))
 
 for i in range(3):
-    mpm.add_cub
+    mpm.add_cube(lower_corner=[0.2 + i * 0.1, 0.3 + i * 0.1],
+                 cube_size=[0.1, 0.1],
+                 material=MPMSolver.material_elastic)
+
+for frame in range(500):
+    mpm.step(

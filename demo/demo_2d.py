ecba5700210dde7d2c26d689ac9ee62b64d22504
moirao <90628406+moirao@users.noreply.github.com>
2021-11-11 08:40:57 +0000

    Update timeline.md

--- demo/demo_2d.py
+++ demo/demo_2d.py
@@ -19,4 +19,13 @@ args = parse_args()
 
 write_to_disk = args.out_dir is not None
 if write_to_disk:
-    os.mkdir(f'{arg
+    os.mkdir(f'{args.out_dir}')
+
+ti.init(arch=ti.cuda)  # Try to run on GPU
+
+gui = ti.GUI("Taichi Elements", res=512, background_color=0x112F41)
+
+mpm = MPMSolver(res=(128, 128))
+
+for i in range(3):
+    mpm.add_cub

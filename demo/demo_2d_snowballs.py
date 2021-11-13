9e0a73e8995d516c5c6271e9bf933d0bc0b5f098
moirao <90628406+moirao@users.noreply.github.com>
2021-11-13 06:02:00 +0000

    Update readme with installation instructions

--- demo/demo_2d_snowballs.py
+++ demo/demo_2d_snowballs.py
@@ -1,3 +1,10 @@
 import taichi as ti
 import numpy as np
-im
+import utils
+from engine.mpm_solver import MPMSolver
+
+write_to_disk = False
+
+ti.init(arch=ti.cuda)  # Try to run on GPU
+
+gui = ti.GUI("Taichi Elements", res=512, ba

5d307d079a73c14b626de5bc37231b5c9f3a6511
moirao <90628406+moirao@users.noreply.github.com>
2021-11-14 11:13:47 +0000

    Update /out

--- demo/demo_2d_sparse_active_blocks.py
+++ demo/demo_2d_sparse_active_blocks.py
@@ -1,2 +1,12 @@
 import os
 import taichi as ti
+import numpy as np
+import utils
+import math
+from engine.mpm_solver import MPMSolver
+import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parse

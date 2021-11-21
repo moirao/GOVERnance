2531d53f2669d11dc6102803eaf3a9a109b78f40
moirao <90628406+moirao@users.noreply.github.com>
2021-11-21 19:48:47 +0000

    x86/mm: account for PGDIR_SIZE alignment

--- demo/demo_3d_bunnies.py
+++ demo/demo_3d_bunnies.py
@@ -3,3 +3,12 @@ import math
 import time
 import numpy as np
 import os
+from utils import Tee
+from engine.mpm_solver import MPMSolver
+import argparse
+from engine.mesh_io import load_mesh
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_

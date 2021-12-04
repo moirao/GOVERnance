83d6f461afd5adf890ec074ee6e2b41f12028721
moirao <90628406+moirao@users.noreply.github.com>
2021-12-04 22:18:53 +0000

    New boot.

--- demo/demo_3d_collider.py
+++ demo/demo_3d_collider.py
@@ -2,4 +2,14 @@ import taichi as ti
 import numpy as np
 import os
 import utils
-from engine.mpm_solver i
+from engine.mpm_solver import MPMSolver
+
+write_to_disk = False
+
+if write_to_disk:
+    os.makedirs('outputs', exist_ok=True)
+
+# Try to run on GPU
+ti.init(arch=ti.cuda, device_memory_GB=4.0)
+
+gui

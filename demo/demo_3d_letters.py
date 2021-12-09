8df3952373ab57b0e5e67fa430b26c1dda928436
moirao <90628406+moirao@users.noreply.github.com>
2021-12-09 06:04:38 +0000

    [ci skip] lint

--- demo/demo_3d_letters.py
+++ demo/demo_3d_letters.py
@@ -9,4 +9,12 @@ from utils import create_output_folder
 from engine.mpm_solver import MPMSolver
 
 with_gui = True
-write_to_disk = T
+write_to_disk = True
+
+# Try to run on GPU
+ti.init(arch=ti.cuda, kernel_profiler=True, device_memory_fraction=0.7)
+
+max_num_particles = 10000000
+
+if with_gui:
+    gui = ti.GUI("MLS-MPM", res=512, background_colo

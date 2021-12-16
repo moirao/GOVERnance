8c933a765a03ae6ccb5a270949aeecd356d78ff6
moirao <90628406+moirao@users.noreply.github.com>
2021-12-16 12:54:27 +0000

    Pulled the latest platform

--- demo/demo_3d_letters2.py
+++ demo/demo_3d_letters2.py
@@ -5,4 +5,13 @@ import numpy as np
 from plyfile import PlyData, PlyElement
 import os
 import utils
-from utils import create_o
+from utils import create_output_folder
+from engine.mpm_solver import MPMSolver
+
+with_gui = True
+write_to_disk = True
+
+# Try to run on GPU
+ti.init(arch=ti.cuda,
+        kernel_profiler=True,
+ 

1edcfba4c9d6096b1654533836634f12291cd92c
moirao <90628406+moirao@users.noreply.github.com>
2021-12-19 12:53:52 +0000

    Added exception

--- demo/demo_3d_letters2.py
+++ demo/demo_3d_letters2.py
@@ -50,4 +50,9 @@ def load_mesh(fn, scale, offset):
 # Use 512 for final simulation/render
 R = 256
 
-mpm = MPMSolver(res=(R, R, R), size=1, unbounded=True, dt_scale=0.5, E_scal
+mpm = MPMSolver(res=(R, R, R), size=1, unbounded=True, dt_scale=0.5, E_scale=8)
+
+mpm.add_surface_collider(point=(0, 0, 0),
+                         normal=(0, 1, 0),
+                         surface=mpm.surface_slip,
+                         

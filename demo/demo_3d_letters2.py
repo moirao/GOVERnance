fb048a965149e120e84b1988f48011bf28ab3a9c
moirao <90628406+moirao@users.noreply.github.com>
2021-12-26 07:48:53 +0000

    vb

--- demo/demo_3d_letters2.py
+++ demo/demo_3d_letters2.py
@@ -114,4 +114,7 @@ for frame in range(15000):
         particles = mpm.particle_info()
         visualize(particles)
 
-    if write_to_di
+    if write_to_disk:
+        mpm.write_particles(f'{output_dir}/{frame:05d}.npz')
+    print(f'Frame total time {time.time() - t:.3f}')
+    print(f'Total running time {time.time() - start_t:.3f}')

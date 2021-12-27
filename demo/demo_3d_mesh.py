76dd95d2e1b0c9f6456772a69b9f4762ce773bb7
moirao <90628406+moirao@users.noreply.github.com>
2021-12-27 11:47:44 +0000

    Modified setup.py

--- demo/demo_3d_mesh.py
+++ demo/demo_3d_mesh.py
@@ -8,4 +8,9 @@ write_to_disk = False
 # Try to run on GPU
 ti.init(arch=ti.cuda, device_memory_GB=3.0)
 
-gui = ti.GUI("Taichi Ele
+gui = ti.GUI("Taichi Elements", res=512, background_color=0x112F41)
+
+mpm = MPMSolver(res=(64, 64, 64), size=1)
+
+triangles = np.fromfile('suzanne.npy', dtype=np.float32)
+triangles = np.reshape(triangle

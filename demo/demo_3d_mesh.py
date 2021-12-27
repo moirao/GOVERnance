713683ab2fdbc9a6750f5f016f96c61a4c98571c
moirao <90628406+moirao@users.noreply.github.com>
2021-12-27 07:21:47 +0000

    drivers: lpm-levels: Provide API to set wake up time from suspend

--- demo/demo_3d_mesh.py
+++ demo/demo_3d_mesh.py
@@ -1,1 +1,11 @@
-impor
+import taichi as ti
+import numpy as np
+import utils
+from engine.mpm_solver import MPMSolver
+
+write_to_disk = False
+
+# Try to run on GPU
+ti.init(arch=ti.cuda, device_memory_GB=3.0)
+
+gui = ti.GUI("Taichi Ele

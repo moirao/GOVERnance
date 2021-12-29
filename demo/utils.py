f8ca56a0b4613442455cd8be7e98c7c7e041aea3
moirao <90628406+moirao@users.noreply.github.com>
2021-12-29 18:51:50 +0000

    Add authors

--- demo/utils.py
+++ demo/utils.py
@@ -4,4 +4,13 @@ from datetime import datetime
 
 demo_dir = os.path.dirname(os.path.abspath(__file__))
 taichi_elements_path = os.path.dirname(demo_dir)
-sys.path.append(taichi_el
+sys.path.append(taichi_elements_path)
+
+
+def create_output_folder(prefix):
+    folder = prefix + '_' + datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    os.mkdir(folder)
+    return folder
+
+
+class Tee(o

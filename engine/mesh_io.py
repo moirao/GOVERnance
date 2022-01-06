e48f96f27e48c0beac791983cb06efc29c4e2122
moirao <90628406+moirao@users.noreply.github.com>
2022-01-06 16:39:45 +0000

    Pontoon: Update Chinese (Taiwan) (zh-TW) localization of Common Voice

--- engine/mesh_io.py
+++ engine/mesh_io.py
@@ -5,3 +5,9 @@ import numpy as np
 def load_mesh(fn, scale=1, offset=(0, 0, 0)):
     if isinstance(scale, (int, float)):
         scale = (scale, scale, scale)
+    print(f'loading {fn}')
+    plydata = PlyData.read(fn)
+    x = plydata['vertex']['x']
+    y = plydata['vertex']['y']
+    z = plydata['vertex']['z']
+    elements = plyda

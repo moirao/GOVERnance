95606a07ff95145c75f3ccdcdbaaaf3b8a38b6ce
moirao <90628406+moirao@users.noreply.github.com>
2022-01-09 08:47:01 +0000

    Update .profile

--- engine/mpm_solver.py
+++ engine/mpm_solver.py
@@ -1,1 +1,14 @@
-im
+import taichi as ti
+import numpy as np
+import time
+import numbers
+import math
+import multiprocessing as mp
+
+USE_IN_BLENDER = False
+
+
+# TODO: water needs Jp - fix this.
+
+
+@ti

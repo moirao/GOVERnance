53cdb1a2288b7fcc8553f27f9c13614e86764246
moirao <90628406+moirao@users.noreply.github.com>
2022-05-13 10:02:38 +0000

    rrrr

--- engine/renderer.py
+++ engine/renderer.py
@@ -3,4 +3,15 @@ import numpy as np
 import math
 import time
 from engine.renderer_utils import out_dir, ray_aabb_intersection, inf, eps, \
-  intersect_sphere, sphere_aabb_intersect_motion, inside_tai
+  intersect_sphere, sphere_aabb_intersect_motion, inside_taichi
+
+from engine.particle_io import ParticleIO
+
+res = 1280, 720
+aspect_ratio = res[0] / res[1]
+
+max_ray_depth = 4
+use_directional_light = True
+
+dist_limit = 100
+# TODO: why doesn'

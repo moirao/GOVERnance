b90809b53382310f5f3b7162b0ef97dbb6c2b5f1
moirao <90628406+moirao@users.noreply.github.com>
2022-12-19 21:14:15 +0000

    Create autoleave.lua

--- tests/test_2d.py
+++ tests/test_2d.py
@@ -3,4 +3,11 @@ import utils
 import math
 from engine import mpm_solver
 
-ti.init(
+ti.init(arch=ti.cuda,
+        device_memory_GB=1.0)  # Try to run on GPU
+mpm = mpm_solver.MPMSolver(res=(24, 24))
+
+for frame in range(5):
+    mpm.step(8e-3)
+    mpm.add_cube(lower_corner=[0.3, 0.7],
+  

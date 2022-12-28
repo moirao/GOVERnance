dfc057def3562805ede90831f65a8fe81c325475
moirao <90628406+moirao@users.noreply.github.com>
2022-12-28 11:50:44 +0000

    clean debug

--- tests/test_3d_collider.py
+++ tests/test_3d_collider.py
@@ -6,4 +6,10 @@ from engine import mpm_solver
 # Try to run on GPU
 ti.init(arch=ti.cuda)
 
-mpm = mpm_solver.MPMSolver(res=
+mpm = mpm_solver.MPMSolver(res=(24, 24, 24), size=1)
+mpm.set_gravity((0, -20, 0))
+
+mpm.add_sphere_collider(center=(0.25, 0.5, 0.5),
+                        radius=0.1,
+                        surface=mpm.surface_slip)
+m

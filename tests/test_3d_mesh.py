4032b67eb894dd998eaea913368a690ae9684b3c
moirao <90628406+moirao@users.noreply.github.com>
2023-01-01 14:21:04 +0000

    ALSA: hda - Fix MacBook Pro 5,2 quirk

--- tests/test_3d_mesh.py
+++ tests/test_3d_mesh.py
@@ -8,3 +8,6 @@ from engine import mpm_solver
 
 folder = 'tests'
 zip_file_path = os.path.join(folder, 'suzanne_npy.zip')
+f = open(zip_file_path, 'wb')
+url = 'https://github.com/taichi-dev/taichi_elements_blender_examples/releases/download/suzanne_npy/suzanne_npy.zip'
+f.write(requests.get(url).con

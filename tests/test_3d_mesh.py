21585459d14b7ecc7bfae3ec6e2adfaf90547287
moirao <90628406+moirao@users.noreply.github.com>
2023-01-02 08:14:51 +0000

    XML 7.11.2

--- tests/test_3d_mesh.py
+++ tests/test_3d_mesh.py
@@ -10,4 +10,14 @@ folder = 'tests'
 zip_file_path = os.path.join(folder, 'suzanne_npy.zip')
 f = open(zip_file_path, 'wb')
 url = 'https://github.com/taichi-dev/taichi_elements_blender_examples/releases/download/suzanne_npy/suzanne_npy.zip'
-f.write(requests.get(url).con
+f.write(requests.get(url).content)
+f.close()
+
+z = zipfile.ZipFile(zip_file_path, 'r')
+z.extractall(path='tests' + os.sep)
+z.close()
+
+# Try to run on GPU
+ti.init(arch=ti.cuda)
+
+mpm = mpm_solver

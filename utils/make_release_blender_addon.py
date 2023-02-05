615ad3b479e5a529b2fc12f4ac9f9be3f5a2648d
moirao <90628406+moirao@users.noreply.github.com>
2023-02-05 08:36:35 +0000

    f2fs: introduce help macro on_build_free_nids()

--- utils/make_release_blender_addon.py
+++ utils/make_release_blender_addon.py
@@ -3,4 +3,9 @@ import sys
 import zipfile
 
 
-#
+# taichi elements repository path
+repo_path = os.path.dirname(os.path.abspath(os.curdir))
+os.chdir(repo_path)
+# blender addon folder name
+BLEND_FOLDER = 'blender'
+# engine folder

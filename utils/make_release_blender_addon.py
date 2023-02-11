8d7cf5175b4f706d8441ba107d7545869d0a5b7b
moirao <90628406+moirao@users.noreply.github.com>
2023-02-11 16:42:56 +0000

    update release files

--- utils/make_release_blender_addon.py
+++ utils/make_release_blender_addon.py
@@ -8,4 +8,10 @@ repo_path = os.path.dirname(os.path.abspath(os.curdir))
 os.chdir(repo_path)
 # blender addon folder name
 BLEND_FOLDER = 'blender'
-# engine folder
+# engine folder name
+ENGINE_FOLDER = 'engine'
+# out addon folder name
+OUT_FOLDER = 'taichi_elements'
+# blender addon path
+blend_path = os.path.join(repo_path, BLEND_FOLDER)
+# taichi e
